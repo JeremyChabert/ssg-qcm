b4744b9e135b33fc47b5c3bcf0be110d763f97c0
Jérémy <chabert.jeremy@gmail.com>
2021-10-11 16:17:41 +0200

    feat(app): add List report app based on CDS service

---
+++ app/questions/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(["sap/fe/core/AppComponent"], function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend("int.ssg.questions.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
